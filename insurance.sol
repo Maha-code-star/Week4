b180d4c8994fcdedf717b4c2b6d83ac0f9ecd673
Maha-code-star <74078213+Maha-code-star@users.noreply.github.com>
2021-03-07 20:59:07 +0300

    Update insurance.sol

--- insurance.sol
+++ insurance.sol
@@ -3,18 +3,6 @@ pragma solidity >=0.6.0 <0.8.1;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-/**
- * @title Tech Insurance tor
- * @dev 
- * Step1: Complete the functions in the insurance smart contract
- * Step2:Add any required methods that are needed to check if the function are called correctly, 
- * and also add a modifier function that allows only the owner can run the changePrice function.
- * Step3: Add any error handling that may occur in any function
- * Step4: Add a modifer function to check the time if the client insurance is valid.
- * Step5 (opcional): Add a refund function that refunds money back to the client after one week. Guaranteed Money Back Plan.  
- * Step6: implement ERC 721 Token to this contract and change what it needs to be changed. 
- * 
- */
 contract TechInsurance is ERC721("NinjaToekn","Ninja") {
     
     /** 
@@ -80,20 +68,13 @@ contract TechInsurance is ERC721("NinjaToekn","Ninja") {
         require( productIndex[_productIndex].price>=1);
         productIndex[_productIndex].price==_price;
     }
-    
-    /**
-    *@dev 
-    * Every client buys an insurance, 
-    * you need to map the client's address to the id of product to struct client, using (client map)
-    */
+  
     
     function buyInsurance(uint _productIndex) public payable {
         require(productIndex[_productIndex].price == msg.value);
         Client memory NewClient;
         NewClient.isValid=true;
         client[msg.sender][_productIndex]= NewClient;
-        // msg.value should be pointed to the price in the product 
-        // uint price = 
         payable(msg.sender).transfer(msg.value);
     } 
     
